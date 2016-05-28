e1db0c3198be84cb642c98c92dc67f669a6b24d1
martin99199 <S1510564018@students.fh-wels.at>
2016-05-28 07:09:18 +0200

    kleine Anpassungen
    Implementierung Verschlüsseln, anpassen an Coding-Standards

--- Application_verheiratet/FrmMain_LoginForm_PartPachler/FrmMainWindow.cs
+++ Application_verheiratet/FrmMain_LoginForm_PartPachler/FrmMainWindow.cs
@@ -16,13 +16,14 @@ namespace FrmMain_LoginForm_PartPachler
 {
     public partial class FrmMainWindow : Form
     {
-        private const string Passphrase = "HalloLeute";                   
         #region Membervariables and Constants
-        private const string pathDB = @"..\..\cDB.csv"; //cDB...Customer Database
+        
+        private const string PATH_DB = @"..\..\cDB.csv"; //cDB...Customer Database
         public List<Customer> customers = new List<Customer>();
         
         #endregion
 
+        #region Constructor, Load-Event and Initialization
         public FrmMainWindow()
         {
             InitializeComponent();
@@ -42,8 +43,16 @@ namespace FrmMain_LoginForm_PartPachler
             
         }
 
+        /// <summary>
+        /// Loads the Login-Form and enables the Main-Form if the password is correct.
+        /// Otherwise it closes the Form.
+        /// </summary>
+        /// <param name="sender">Form that is loaded</param>
+        /// <param name="e">Form loaded event</param>
         private void FrmMainWindow_Load(object sender, EventArgs e)
         {
+            //Fürs Testen kann die Passwortabfrage auskommentiert werden!!
+            //this.InitializeForm();
             FrmLogIn loginForm = new FrmLogIn();
             DialogResult res = loginForm.ShowDialog();
             if (res == DialogResult.OK)
@@ -59,11 +68,18 @@ namespace FrmMain_LoginForm_PartPachler
                 //sense.
             }
         }
+
+        /// <summary>
+        /// Initializes the Form, therefore checks if the database exists
+        /// and loads the customers into the memory. If an error Occurs this 
+        /// Method closes the form!
+        /// </summary>
+        /// <returns></returns>
         private bool InitializeForm()
         {
             bool initOK = true;
             //Check if File exists:
-            bool dbExists = File.Exists(pathDB);
+            bool dbExists = File.Exists(PATH_DB);
             try
             {
                 if (dbExists)
@@ -87,151 +103,159 @@ namespace FrmMain_LoginForm_PartPachler
 
             return initOK;
         }
+        #endregion
 
-        private void UpdateView()
-        {
-            this.lbxCustomer.Items.Clear();
-            if (this.customers.Count > 0)
-            {
-                this.btnBalance.Enabled = true;
-                this.btnEdit.Enabled = true;
-                this.btnSearch.Enabled = true;
-                this.btnBalance.Enabled = true;
-                this.lbxCustomer.Enabled = true;
-                foreach (Customer c in this.customers)
-                {
-                    this.lbxCustomer.Items.Add(c.ToVisualString());
-                }
-                this.lbxCustomer.SelectedIndex = 0;
-            }
-            else
-            {
-                this.btnBalance.Enabled = false;
-                this.btnEdit.Enabled = false;
-                this.btnSearch.Enabled = false;
-                this.btnBalance.Enabled = false;
-                this.lbxCustomer.Enabled = false;
-                this.lbxCustomer.Items.Add("No Customer stored! Click -Add Customer-");
-            }
-        }
 
-        //Encryption must be implemented!!!!
+
         #region Security Encrypt-Decrypt 
         private string EncryptDB()
         {
             string dbEncrypted = "";
             foreach( Customer cus in this.customers)
             {
-                dbEncrypted += cus.ToString() + "\n";
+                dbEncrypted += EncryptCustomer(cus) + "\n";
             }
 
             return dbEncrypted;
         }
+        /// <summary>
+        /// Verschlüsselt den Customer String nach Cäsar
+        /// </summary>
+        /// <param name="cus">Ein Objekt der Klasse Customer not null!</param>
+        /// <returns>Verschlüsselten Customer</returns>
+        private static string EncryptCustomer(Customer cus)
+        {
+            string customerEncrypted = "";
+            if (cus != null)
+            {
+                const string sAlphabet =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+-/=?^_`{|}~.ÄäöÖüÜ@";    
+                int nSizeAlphabet = sAlphabet.Length;
+                int nIndexChar = 0;
+                string[] fields = new string[] {
+                    cus.CustomerNumber.ToString(),
+                    cus.FirstName,
+                    cus.LastName,
+                    cus.EMailAdress,
+                    cus.DateLastChange.ToString(),
+                    cus.Balancing.ToString()
+                };
 
-        //Should work! -- Not tested yet
-        //private static string EncryptCustomer (Customer cus)
-        //{
-        //    string customerEncrypted = "";
-        //    string[] fields = new string[] {
-        //        cus.CustomerNumber.ToString(),
-        //        cus.FirstName,
-        //        cus.LastName,
-        //        cus.EMailAdress,
-        //        cus.DateLastChange.ToString(),
-        //        cus.Balancing.ToString()
-        //    };
+                foreach (string field in fields)
+                {
+                    foreach (char c in field)
+                    {
+                        nIndexChar = sAlphabet.IndexOf(c);
+                        if (nIndexChar >= 0)
+                        {
+                            //Buchstabe/Zeichen enthalten!
+                            customerEncrypted += sAlphabet[(nIndexChar + 3)  % nSizeAlphabet];
+                            // der Neue Buchstabe ist einfach um 3 Buchstaben versetzt.
+                            // Wird das Ende des Alphabets erreicht wird von vorne wieder begonnen.
+                        }
+                        else
+                        {
+                            //Buchstabe/Zeichen nicht enthalten
+                            customerEncrypted += c;
+                        }
+                    }
 
-        //    foreach (string field in fields)
-        //    {
-        //        #region Old Method:
-        //        //foreach(char c in field)
-        //        //{
-        //        //    char newChar = (char)((c + 2 )%123);
-        //        //    customerEncrypted += newChar;
-        //        //    // A = 65, z = 122
-        //        //}
-        //        #endregion
+                    customerEncrypted += ";";
+                }
+            }
+            return customerEncrypted;
+        }
 
-        //        #region New Method:
-        //        customerEncrypted += StringCipher.Encrypt(field, Passphrase);
-        //        #endregion
-
-        //        customerEncrypted += ";";
-        //    }
-        //    return customerEncrypted;
-        //}
-
-        //Should work!! not tested yet
+        /// <summary>
+        /// Entschlüsselt den Customer aus einem verschlüsselten String
+        /// </summary>
+        /// <param name="input">string, der einen verschlüsselten Customer enthält</param>
+        /// <returns>Customer aus Verschlüsseltem Datensatz hergestellt</returns>
         private static Customer DecryptCustomer(string input)
         {
-            string customerDecrypted = "";
-            string[] fields = input.Split(';');
-            #region Old Method:
-            foreach (char c in input)
+            const string sAlphabet =
+                "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+-/=?^_`{|}~.ÄäöÖüÜ@";    
+            int nSizeAlphabet = sAlphabet.Length;
+            int nIndexChar = 0;
+            string sDecryptCustomerString = "";
+
+            for (int i = 0; i < input.Length; i++)
             {
-                if (c != ';')
+                nIndexChar = sAlphabet.IndexOf(input[i]);
+                if (nIndexChar >= 0)
                 {
-                    customerDecrypted += (char)((c - 2 + 123) % (123));
+                    sDecryptCustomerString += sAlphabet[(nIndexChar - 3 + nSizeAlphabet) % nSizeAlphabet];
                 }
                 else
                 {
-                    customerDecrypted += ';';
+                    sDecryptCustomerString += input[i];
                 }
             }
-            #endregion
-
-            customerDecrypted = input;
-
-            return GetCustomerFromInput(customerDecrypted);
+            return GetCustomerFromInput(sDecryptCustomerString);
         }
         #endregion
 
         #region Read and Write Customer DB
         private void WriteCustomerDB()
         {
+            StreamWriter file = new StreamWriter(PATH_DB);
             try
             {
-                StreamWriter file = new StreamWriter(pathDB);
                 file.Write(this.EncryptDB());
                 file.Close();
             }
             catch
             {
                 MessageBox.Show("Error: Writing not possible!");
+                file.Close();
             }
         }
         private List<Customer> ReadCustomerDB()
         {
-            StreamReader file = new StreamReader(pathDB);
+            StreamReader file = new StreamReader(PATH_DB);
             string line = "";
             List<Customer> cList = new List<Customer>();
             while(file.Peek()>-1)
             {
                 line = file.ReadLine();
-                //line = StringCipher.Decrypt(line, Passphrase);
                 cList.Add(DecryptCustomer(line));
             }
             file.Close();
             return cList; 
         }
-#endregion
 
+        /// <summary>
+        /// Generates a new Cutstomer out of a special string
+        /// </summary>
+        /// <param name="input">string that follows the convention:
+        ///                     'CustomerNumber';'FirstName';'LastName';'EmailAdress';'DateLastChanged';'Balancing'</param>
+        /// <returns></returns>
         private static Customer GetCustomerFromInput(string input)
         {
-            string[] fields = input.Split(';');
-            int CustomerNumber = Convert.ToInt32(fields[0]);
-            string rFirstName = fields[1];
-            string rLastName = fields[2];
-            string rEmailAdress = fields[3];
-            DateTime rDateLastChanged = Convert.ToDateTime(fields[5]);
-            double rBalancing = Convert.ToDouble(fields[4]);
-            return new Customer(rFirstName, rLastName, rEmailAdress, rDateLastChanged, rBalancing);
+            if (!string.IsNullOrEmpty(input))
+            {
+                string[] fields = input.Split(';');
+                int CustomerNumber = Convert.ToInt32(fields[0]);
+                string rFirstName = fields[1];
+                string rLastName = fields[2];
+                string rEmailAdress = fields[3];
+                DateTime rDateLastChanged = Convert.ToDateTime(fields[4]);
+                double rBalancing = Convert.ToDouble(fields[5]);
+                return new Customer(rFirstName, rLastName, rEmailAdress, rDateLastChanged, rBalancing);
+            }
+            else
+            {
+                throw new ArgumentException("Falsches Datenformat bei input in GetCustomerFromInput(input)");
+            }          
         }
+        #endregion
+
+        
 
         #region Click- Events:
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            if((tbxSearch.Text != null && tbxSearch.Text.Length >0) || lbxCustomer.SelectedIndex == 3)
+            if((tbxSearch.Text != null && tbxSearch.Text.Length >0) || lbxCustomer.SelectedIndex == 3) // Index 3 = Search for DateLastChanged
             {
                 bool found = false;
                 this.lbxCustomer.SelectedIndices.Clear();
@@ -246,7 +270,7 @@ namespace FrmMain_LoginForm_PartPachler
                         {
                             string fullName1 = customers[i].FirstName + " " + customers[i].LastName;
                             string fullName2 = customers[i].LastName + " " + customers[i].FirstName;
-                            if (fullName1 == tbxSearch.Text || fullName2 == tbxSearch.Text)
+                            if (fullName1.ToLower() == tbxSearch.Text.ToLower() || fullName2.ToLower() == tbxSearch.Text.ToLower())
                             {
                                 this.lbxCustomer.SelectedIndices.Clear();
                                 this.lbxCustomer.SelectedIndices.Add(i);
@@ -362,6 +386,7 @@ namespace FrmMain_LoginForm_PartPachler
         }
         #endregion
 
+        #region Other Form Events
         private void cbxSearch_SelectedIndexChanged(object sender, EventArgs e)
         {
             if (cbxSearch.SelectedIndex == 3)
@@ -375,12 +400,32 @@ namespace FrmMain_LoginForm_PartPachler
                 this.tbxSearch.Enabled = true;
             }
         }
-
-
-
-        
-
-
-       
+        private void UpdateView()
+        {
+            this.lbxCustomer.Items.Clear();
+            if (this.customers.Count > 0)
+            {
+                this.btnBalance.Enabled = true;
+                this.btnEdit.Enabled = true;
+                this.btnSearch.Enabled = true;
+                this.btnBalance.Enabled = true;
+                this.lbxCustomer.Enabled = true;
+                foreach (Customer c in this.customers)
+                {
+                    this.lbxCustomer.Items.Add(c.ToVisualString());
+                }
+                this.lbxCustomer.SelectedIndex = 0;
+            }
+            else
+            {
+                this.btnBalance.Enabled = false;
+                this.btnEdit.Enabled = false;
+                this.btnSearch.Enabled = false;
+                this.btnBalance.Enabled = false;
+                this.lbxCustomer.Enabled = false;
+                this.lbxCustomer.Items.Add("No Customer stored! Click -Add Customer-");
+            }
+        }
+        #endregion
     }
 }
